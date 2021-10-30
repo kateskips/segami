b8ee8a52df2cdf76f68d3b69e49c24046ece1f6f
Kate Follett <git@klgf.dev>
2021-10-30 16:11:53 -0400

    mapped out the images

--- segami/src/Carousel.js
+++ segami/src/Carousel.js
@@ -1,9 +1,14 @@
 import React, {Component} from "react";
+import ImageData from "./imageData.json";
 
 class Carousel extends Component {
     render(){
         return(
-            <div>Carousel</div>
+            <div className='round'>
+                {ImageData.map((imagePhoto, index) => {
+                    return <img src={imagePhoto.image} alt='ten hundred art' />
+                })}
+            </div>
         )
     }
 }
