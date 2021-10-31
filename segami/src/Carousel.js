3c3c5ea9e0c8330d87589d3836debad068c304c2
Kate Follett <git@klgf.dev>
2021-10-30 20:59:57 -0400

    switching to hooks instead and added styled

--- segami/src/Carousel.js
+++ segami/src/Carousel.js
@@ -1,17 +1,33 @@
-import React, {Component} from "react";
+import React  from "react";
 import ImageData from "./imageData.json";
+import styled from 'styled-components'
 
-class Carousel extends Component {
-    render(){
-        return(
-            <div className='round'>
-                {ImageData.map((imagePhoto, index) => {
-                    return <img src={imagePhoto.image} alt='ten hundred art' className="image" />
-                })}
-                <button>Next</button> <button>Prev</button>
-            </div>
-        )
-    }
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+  
+`;
+
+
+const Carousel = () => {
+    return(
+        <div className='image-round'>
+            {ImageData.map((imagePhoto, index) => {
+                return <img src={imagePhoto.image} alt='ten hundred art' className="image" />
+            })}
+            <Button>Next</Button><Button>Prev</Button>
+        </div>
+    )
 }
+    
+       
+    
+
 
 export default Carousel;
