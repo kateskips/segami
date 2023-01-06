a7105fae16dbd9afe38e6f9a2153625525c85cda
Kate Follett <git@klgf.dev>
2023-01-06 15:01:23 +0800

    fixed spacing

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -2,17 +2,7 @@ import React, { useState } from "react";
 import { Button, Space } from 'antd';
 
 
-/*const Button = styled.button`
-  display: inline-block;
-  color: palevioletred;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  display: block;
-`;
-*/
+
 
 //ImageList is itself a type
 // ImagesList is a type of array of objects, in that array each object
@@ -45,24 +35,24 @@ export function Carousel(props: { images: ImagesList }) {
             <div className="buttons">
               {/* When the either button is pressed we rotate the array*/}
               <Space>
-              <Button type='primary'className="next-button" onClick={e => {
-                // rotate the array, moving the first to the end.
-                setImages(([first, ...rest]) => [...rest, first])
-              }}>Next</Button>
-              <Button type='primary'className="prev-button" onClick={e => {
-                // rotate the array, moving the last to the beginning.
-                //
-                // I want to write
-                //
-                //   setImages(([...rest, last]) => [last, ...rest])
-                //
-                // but javascript doesn't yet allow this.
-                setImages((oldImages) => {
-                  const last = oldImages[oldImages.length - 1];
-                  const rest = oldImages.slice(0, -1)
-                  return [last, ...rest]
-                })
-              }}>Prev</Button>
+                <Button type='primary' className="next-button" onClick={e => {
+                  // rotate the array, moving the first to the end.
+                  setImages(([first, ...rest]) => [...rest, first])
+                }}>Next</Button>
+                <Button type='primary' className="prev-button" onClick={e => {
+                  // rotate the array, moving the last to the beginning.
+                  //
+                  // I want to write
+                  //
+                  //   setImages(([...rest, last]) => [last, ...rest])
+                  //
+                  // but javascript doesn't yet allow this.
+                  setImages((oldImages) => {
+                    const last = oldImages[oldImages.length - 1];
+                    const rest = oldImages.slice(0, -1)
+                    return [last, ...rest]
+                  })
+                }}>Prev</Button>
               </Space>
             </div>
           </>
