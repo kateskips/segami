ca5c2dce088fbe78f86f5ae83c7624d179c88879
Kate Follett <git@klgf.dev>
2023-01-06 13:43:42 +0800

    Added titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
 import { Carousel, ImagesList } from './Carousel';
 import './App.css';
 import Data from './imageData.json';
 
 const { Content } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
     <>
       <Layout>
         <center>
+        <Title>Segami</Title>
+        <Title level={4}>An image carousel of Ten Hundred's Artwork</Title>
         <Content> 
           <Carousel images={Data} />
           <Carousel images={[] as ImagesList} />
